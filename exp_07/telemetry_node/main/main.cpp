99509582500f06cc12129f2d3dd17fbb41e3ac19
ThiwankaS <thivankas@outlook.com>
2026-05-14 21:01:19 +0300

    simple custome thread creating on a ESP32

---
+++ exp_07/telemetry_node/main/main.cpp
@@ -0,0 +1,15 @@
+#include "MonitorTask.hpp"
+#include "freertos/idf_additions.h"
+#include "portmacro.h"
+#include <iostream>
+
+extern "C" void app_main(void) {
+    std::cout << "--- Telemetry Node --- \n";
+    auto* monitor = new MonitorTask("Sys_monitor", 4096, 1);
+    monitor->start(0);
+    
+    // main thread will be live ideal as an observer
+    while (true) {
+        vTaskDelay(portMAX_DELAY); 
+    }
+}
